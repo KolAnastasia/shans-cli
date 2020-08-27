92cacf63fa2e63f8e02f92c5fa60d6ad07c153be
Kol Nastya <nastya_grig.2015@mail.ru>
2020-08-27 16:36:04 +0300

    build fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
     "rules": {
       "no-mixed-spaces-and-tabs": 0, // disable rule
     },
+    "parser": "vue-eslint-parser",
     "parserOptions": {
-      "sourceType": "module",
-      "ecmaVersion": 2015
+      'parser': 'babel-eslint',
+    'ecmaVersion': 2020,
+    'sourceType': 'module'
     }
 }
